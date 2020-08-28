792e976b1f6b0162429784b4e7828c1c5ccccf60
Manoj Pathak <mapathak@chegg.com>
2020-08-28 23:10:09 +0530

    View Layout

---
+++ components/DetailsView/DetailsView.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function DetailsView() {
+  return (<h1>Details View</h1>);
+}
+
+export default DetailsView;
---
+++ components/ListView/ListView.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ListViewContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ListContainer = styled.div`
+  border: 1px green solid;
+  flex: 1;
+`;
+
+function ListView() {
+  return (
+    <ListViewContainer>
+      <div>
+        <h1>Filter</h1>
+      </div>
+      <ListContainer>
+
+      </ListContainer>
+    </ListViewContainer>
+  );
+}
+
+export default ListView;
